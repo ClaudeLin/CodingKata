803caace8d326d247eb647dde6c44a603784725a
ClaudeLin <linc27595@gmail.com>
2017-08-02 15:07:06 +0800

    Init Project and add first test

---
+++ CodingKata/StringAverage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingKata
+{
+    public class StringAverage
+    {
+	    public string GetStringAverage(string inputString)
+	    {
+			return "four";
+	    }
+    }
+}
---
+++ CodingKataTest/StringAverageTest.cs
@@ -0,0 +1,28 @@
+﻿using CodingKata;
+using NUnit.Framework;
+
+namespace CodingKataTest
+{
+	[TestFixture]
+	public class StringAverageTest
+	{
+		[Test]
+		[TestCase("zero nine five two", "four")]
+		public void TestAverageString(string inputString, string expected)
+		{
+			var target=new StringAverage();
+
+			var actual = target.GetStringAverage(inputString);
+
+			Assert.AreEqual(expected, actual);
+
+			//Assert.AreEqual("four", Kata.AverageString("zero nine five two"));
+			//Assert.AreEqual("three", Kata.AverageString("four six two three"));
+			//Assert.AreEqual("three", Kata.AverageString("one two three four five"));
+			//Assert.AreEqual("four", Kata.AverageString("five four"));
+			//Assert.AreEqual("zero", Kata.AverageString("zero zero zero zero zero"));
+			//Assert.AreEqual("two", Kata.AverageString("one one eight one"));
+			//Assert.AreEqual("n/a", Kata.AverageString(""));
+		}
+	}
+}
