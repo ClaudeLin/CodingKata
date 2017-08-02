b619ab2215061a18b405256f7bf50ef918ec4c59
ClaudeLin <linc27595@gmail.com>
2017-08-02 16:31:40 +0800

    refactoring: change enum to dictionary

--- CodingKata/StringAverage.cs
+++ CodingKata/StringAverage.cs
@@ -6,20 +6,36 @@ namespace CodingKata
 {
 	public class StringAverage
 	{
+		private readonly Dictionary<string,int> stringIntMapping=new Dictionary<string, int>()
+		{
+			{"zero",0},
+			{"one",1},
+			{"two",2},
+			{"three",3},
+			{"four",4},
+			{"five",5},
+			{"six",6},
+			{"seven",7},
+			{"eight",8},
+			{"nine",9}
+		};
+
+		private const string _emptyInputValue = "n/a";
 
 		public string GetStringAverage(string inputString)
 		{
 			if (string.IsNullOrEmpty(inputString))
 			{
-				return "n/a";
+				return _emptyInputValue;
 			}
-			var stringNums = ConvertToInt(inputString);
-			return Enum.GetName(typeof(EnumStringNumberMapping), stringNums.Sum() / stringNums.Count);
+			var stringNums = ConvertStringToInt(inputString);
+
+			return stringIntMapping.FirstOrDefault(x => x.Value == stringNums.Sum()/stringNums.Count).Key;
 		}
 
-		private List<int> ConvertToInt(string inputString)
+		private List<int> ConvertStringToInt(string inputString)
 		{
-			return inputString.ToLower().Split(' ').Select(str => (int)Enum.Parse(typeof(EnumStringNumberMapping), str)).ToList();
+			return inputString.ToLower().Split(' ').Select(str => stringIntMapping[str]).ToList();
 		}
 	}
 }
