36737c27865001801ff67adb89f846cbd0e4ed40
ClaudeLin <linc27595@gmail.com>
2017-08-14 16:38:28 +0800

    1. add new kata BandNameGenerator 2. add first 2 tests

---
+++ CodingKata/BandNameGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingKata
+{
+    public class BandNameGenerator
+    {
+        public string GenerateBandName(string inputString)
+        {
+            return "";
+        }
+    }
+}
---
+++ CodingKataTest/BandNameGeneratorTest.cs
@@ -0,0 +1,22 @@
+﻿using CodingKata;
+using NUnit.Framework;
+
+namespace CodingKataTest
+{
+    [TestFixture]
+    public class BandNameGeneratorTest
+    {
+        [Test]
+        [TestCase("", "")]
+        [TestCase(null, "")]
+        public void Input_Empty_String_Should_Be_Empty(string inputString, string expected)
+        {
+            var target = new BandNameGenerator();
+
+            var actual = target.GenerateBandName(inputString);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
