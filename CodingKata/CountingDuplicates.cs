ac6b051bdedf0c43ca4285e5d9f4bd44173433ce
ClaudeLin <linc27595@gmail.com>
2017-08-16 16:23:21 +0800

    1. add new kata ->Counting Duplicates 2. add first test and pass it

---
+++ CodingKata/CountingDuplicates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingKata
+{
+    public class CountingDuplicates
+    {
+        public int CountDuplicateChar(string inputString)
+        {
+            return 0;
+        }
+    }
+}
---
+++ CodingKataTest/CountingDuplicatesTest.cs
@@ -0,0 +1,26 @@
+﻿using CodingKata;
+using NUnit.Framework;
+
+namespace CodingKataTest
+{
+    [TestFixture]
+    public class CountingDuplicatesTest
+    {
+        [Test]
+        [TestCase("", 0)]
+        //[TestCase(null, 0)]
+        //[TestCase("abcde", 0)]
+        //[TestCase("aabbcde", 2)]
+        //[TestCase("aabBcde", 2,"","should ignore case")]
+        //[TestCase("Indivisibility", 1)]
+        //[TestCase("Indivisibilities", 2,"", "characters may not be adjacent")]
+        public void Input_String_Counting_Duplicates_As_expected(string inputString, int expected)
+        {
+            var target = new CountingDuplicates();
+
+            var actual = target.CountDuplicateChar(inputString);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
