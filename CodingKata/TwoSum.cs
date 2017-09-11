e6f30dde733ee2f9c677af5e087a408efef178d1
ClaudeLin <linc27595@gmail.com>
2017-09-12 00:19:13 +0800

    Add new Kata -> TwoSum

---
+++ CodingKata/TwoSum.cs
@@ -0,0 +1,54 @@
+﻿namespace CodingKata
+{
+    public class TwoSum
+    {
+        private int _inputNumsIndex;
+        private int[] _inputNums;
+        private bool _alreadyFoundAns;
+        private int[] _twoSumAns;
+        private int _targetNum;
+        private int _remainNum;
+
+        public int[] GetTwoSumAns(int[] inputNums, int targetNum)
+        {
+            InitData(inputNums, targetNum);
+            while (!_alreadyFoundAns)
+            {
+                UpdateRemainNum();
+                SearchTwoSumAns();
+            }
+
+            return _twoSumAns;
+        }
+
+        private void InitData(int[] inputNums, int targetNum)
+        {
+            _remainNum = targetNum;
+            _targetNum = targetNum;
+            _inputNums = inputNums;
+            _inputNumsIndex = -1;
+            _twoSumAns = new int[2];
+            _alreadyFoundAns = false;
+        }
+
+        private void UpdateRemainNum()
+        {
+            _inputNumsIndex++;
+            _remainNum = _targetNum - _inputNums[_inputNumsIndex];
+           
+        }
+
+        private void SearchTwoSumAns()
+        {
+            for (var i = _inputNumsIndex + 1; i < _inputNums.Length; i++)
+            {
+                if (_remainNum == _inputNums[i])
+                {
+                    _twoSumAns = new[] {_inputNumsIndex, i};
+                    _alreadyFoundAns = true;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ CodingKataTest/TwoSumTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using CodingKata;
+using NUnit.Framework;
+
+namespace CodingKataTest
+{
+    [TestFixture]
+    public class TwoSumTest
+    {
+        private TwoSum target;
+        private int[] expected;
+        private int[] inputNums;
+        private int targetNum;
+        private int[] actual;
+        [Test]
+        public void Input_0_1_2_Target_1_Should_Be_0_1()
+        {
+            target = new TwoSum();
+            expected = new[] { 0, 1 };
+            inputNums =  new [] { 0,1,2 };
+            targetNum = 1;
+
+            actual = target.GetTwoSumAns(inputNums, targetNum);
+            
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Input_1_2_5_6_Target_11_Should_Be_2_3()
+        {
+            target = new TwoSum();
+            expected = new[] {2,3};
+            inputNums = new[] {1,2,5,6 };
+            targetNum = 11;
+
+            actual = target.GetTwoSumAns(inputNums, targetNum);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Input_2_3_4_8_9_Target_7_Should_Be_1_2()
+        {
+            target = new TwoSum();
+            expected = new[] { 1,2 };
+            inputNums = new[] { 2,3,4,8,9 };
+            targetNum = 7;
+
+            actual = target.GetTwoSumAns(inputNums, targetNum);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
