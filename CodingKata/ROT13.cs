c393379b172228d4d7de9b3c1826e5d7329f003b
ClaudeLin <linc27595@gmail.com>
2018-02-27 03:16:36 +0800

    kata ROT13 first working version

---
+++ CodingKata/ROT13.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingKata
+{
+    public class ROT13
+    {
+       
+        private string input;
+        private Dictionary<string, string> ROT13LookUp = new Dictionary<string, string>();
+
+        public ROT13(string input)
+        {
+            this.input = input;
+            InitLookUp();
+        }
+
+        private void InitLookUp()
+        {
+            ROT13LookUp.Add("A", "N");
+            ROT13LookUp.Add("B", "O");
+            ROT13LookUp.Add("C", "P");
+            ROT13LookUp.Add("D", "Q");
+            ROT13LookUp.Add("E", "R");
+            ROT13LookUp.Add("F", "S");
+            ROT13LookUp.Add("G", "T");
+            ROT13LookUp.Add("H", "U");
+            ROT13LookUp.Add("I", "V");
+            ROT13LookUp.Add("J", "W");
+            ROT13LookUp.Add("K", "X");
+            ROT13LookUp.Add("L", "Y");
+            ROT13LookUp.Add("M", "Z");
+            ROT13LookUp.Add("N", "A");
+            ROT13LookUp.Add("O", "B");
+            ROT13LookUp.Add("P", "C");
+            ROT13LookUp.Add("Q", "D");
+            ROT13LookUp.Add("R", "E");
+            ROT13LookUp.Add("S", "F");
+            ROT13LookUp.Add("T", "G");
+            ROT13LookUp.Add("U", "H");
+            ROT13LookUp.Add("V", "I");
+            ROT13LookUp.Add("W", "J");
+            ROT13LookUp.Add("X", "K");
+            ROT13LookUp.Add("Y", "L");
+            ROT13LookUp.Add("Z", "M");
+        }
+
+        public string GetResult()
+        {
+            var inputs = this.input.ToCharArray();
+
+            var result = new StringBuilder();
+            string temp;
+            foreach (var inputchar in inputs)
+            {
+                var point = Convert.ToInt32(inputchar);
+                if ((point >= 65 && point <= 90) || (point >= 97 && point <= 122))
+                {
+                    if (char.IsLower(inputchar))
+                    {
+                        if (ROT13LookUp.TryGetValue(inputchar.ToString().ToUpper(), out temp))
+                        {
+                            temp = temp.ToLower();
+                            result.Append(temp);
+                            continue;
+                        }
+                    }
+
+                    result.Append(ROT13LookUp[inputchar.ToString()]);
+                    continue;
+                }
+                result.Append(inputchar);
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ CodingKataTest/ROT13Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodingKata;
+using NUnit.Framework;
+
+namespace CodingKataTest
+{
+    [TestFixture]
+    public class ROT13Test
+    {
+        [Test]
+        [TestCase("","")]
+        [TestCase("A","N")]
+        [TestCase("a","n")]
+        [TestCase("A a","N n")]
+        [TestCase("ROT13 example.", "EBG13 rknzcyr.")]
+        public void TestROT13(string input,string expected)
+        {
+            var target = new ROT13(input);
+
+            var actual = target.GetResult();
+
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
