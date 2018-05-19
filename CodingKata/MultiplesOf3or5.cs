d08522b82f07e1e4f5f58f92e56bf8fdc6df1c0b
ClaudeLin <linc27595@gmail.com>
2018-05-19 17:40:27 +0800

    add new kata MultiplesOf3or5

---
+++ CodingKata/MultiplesOf3or5.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingKata
+{
+    public class MultiplesOf3or5
+    {
+        public int GetMultiplesSum(int input)
+        {
+            var list = new List<int>();
+
+            for (var i = 1; i < input; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    list.Add(i);
+                }
+            }
+
+            return list.Sum();
+        }
+    }
+}
---
+++ CodingKataTest/MultiplesOf3or5Test.cs
@@ -0,0 +1,20 @@
+﻿using CodingKata;
+using NUnit.Framework;
+
+namespace CodingKataTest
+{
+    [TestFixture]
+    public class MultiplesOf3or5Test
+    {
+        [Test]
+        [TestCase(23,10)]
+        [TestCase(60,16)]
+        public void TestMultiplesSum(int expected,int input)
+        {
+            var target = new MultiplesOf3or5();
+            var actual=target.GetMultiplesSum(input);
+
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
