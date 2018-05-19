aa6ef21e1d3844ead33c7f268f416dc36d875689
ClaudeLin <linc27595@gmail.com>
2018-05-19 16:17:52 +0800

    add new kata ReverseWords
    use extension method to finish this kata

---
+++ CodingKata/ReverseWords.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingKata
+{
+    public static class ReverseWords
+    {
+        public static string ReverseEachWords(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+            var words = str.Split(' ');
+            var results = new List<string>();
+            char[] temp;
+            foreach (var word in words)
+            {
+                temp = word.ToCharArray();
+                Array.Reverse(temp);
+                results.Add(new string(temp));
+            }
+            return results.Aggregate(string.Empty, (current, result) => current + " " + result).Substring(1);
+        }
+    }
+}
---
+++ CodingKataTest/ReverseWordsTest.cs
@@ -0,0 +1,21 @@
+﻿using CodingKata;
+using NUnit.Framework;
+
+namespace CodingKataTest
+{
+    [TestFixture]
+    public class ReverseWordsTest
+    {
+        [Test]
+        [TestCase("Hi","iH",TestName = "one word reverse")]
+        [TestCase("sihT si na !elpmaxe", "This is an example!", TestName = "code War Example")]
+        [TestCase("","",TestName = "empty word")]
+        [TestCase(null,null,TestName = "null word")]
+        public void ReverseEachWord(string expected, string input)
+        {
+            var actual = input.ReverseEachWords();
+            
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
